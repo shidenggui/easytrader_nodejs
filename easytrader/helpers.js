8b67014747b807509522cbdf9733397fc49061b4
shidenggui <longlyshidenggui@gmail.com>
2016-02-17 22:21:10 +0800

    use async to control flow

--- easytrader/helpers.js
+++ easytrader/helpers.js
@@ -1,23 +1,25 @@
 "use strict"
 var shelljs = require('shelljs'),
-  path = require('path')
+  path = require('path'),
+  execSync = require('child_process').execSync
 
 
 function recognizeVerifyCode(imagePath, broker) {
   let cmd = path.resolve('./thirdlibrary/')
   switch (broker) {
     case 'ht':
-      cmd += 'yjb_verify_code.jar guojin '
+      cmd += '/getcode_jdk1.5.jar '
       break
     case 'yjb':
-      cmd +='getcode_jdg1.5.jar '
+      cmd += '/yjb_verify_code.jar guojin '
       break
   }
-  cmd += 'vcode'
+  cmd += imagePath
   const verifyCodeStart = -5
   const verifyCodeLength = 4
+  cmd = `java -jar ${cmd}`
   console.log(cmd)
-  const stdout = shelljs.exec(`java -jar ${cmd}`).output
+  const stdout = execSync(cmd, {encoding: 'utf8'})
   console.log(stdout)
   return stdout.substr(verifyCodeStart, verifyCodeLength)
 }
--- easytrader/yjbtrader.js
+++ easytrader/yjbtrader.js
@@ -1,5 +1,7 @@
 'use strict'
 var fs = require('fs')
+var _ = require('lodash'),
+  async = require('async')
 var WebTrader = require('./webtrader'),
   helpers = require('./helpers')
 
@@ -14,28 +16,51 @@ class YJBTrader extends WebTrader {
   prepare(accountPath) {
     super.readConfig(accountPath)
   }
+
   login() {
-    this.request
-      .get(this.config['login_page'])
-    const verifyCode = this.handleRecognizeCode()
-    let form = this.accountConfig['login']
-    
+    async.waterfall([
+      callback => {
+        this.request
+          .get(this.config['login_page'], _ => callback(null))
+      },
+      callback => {
+        let imgPath = 'vcode.jpg'
+        let stream = this.request
+          .get(this.config.verify_code_api)
+          .pipe(fs.createWriteStream(imgPath))
+
+        stream.on('finish', _ => {
+          const verifyCode = helpers.recognizeVerifyCode(imgPath, 'yjb')
+          console.log(verifyCode)
+          fs.unlink(imgPath)
+          callback(null, verifyCode)
+        })
+      },
+      (verifyCode, callback) => {
+        console.log(verifyCode)
+      }
+    ], (err, result) => console.log(err, result))
+
+
+
+    //const verifyCode = this.handleRecognizeCode()
+    //let params = this.config['login']
+    //let other_params = {
+    //  mac_addr: '0000',
+    //  account_content: this.accountConfig.account,
+    //  password: this.accountConfig.password,
+    //  validateCode: verifyCode
+    //}
+    //params = _.assign(params, other_params)
+    //console.log(params)
+    //let login_response = this.request
+    //  .post(this.config.login_api)
+    //  .form(params)
+    //  .on('body', b => console.log(b))
 
   }
 
   handleRecognizeCode() {
-    const imgPath = 'vcode'
-    this.request.get({
-      url: this.config['verify_code_api'],
-      qs: {
-        randomStamp: Math.random()
-      }
-    })
-      .pipe(fs.createWriteStream(imgPath))
-
-    const verifyCode = helpers.recognizeVerifyCode(imgPath, 'ht')
-    fs.unlink(imgPath)
-    return verifyCode
   }
 
   test() {
